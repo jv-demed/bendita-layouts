dd57f3eec2ad1d674ccdc864356342891da4312c
jv-demed <jv.demed@gmail.com>
2023-01-12 14:54:59 -0300

    Feat: Melhora na apresentação

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,35 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const ProcedimentosStyled = styled.main`
+    align-items: center;
+    background-image: linear-gradient(to bottom,#471e4e,#35173b);
+    color: white;
+    display: flex;
+    flex-direction: column;
+    font-family: sans-serif;
+    gap: 10px;
+    height: 100vh;
+    justify-content: center;
+    a{
+        border: 1px solid white;
+        border-radius: 4px;
+        color: white;
+        font-size: 1.2rem;
+        padding: 5px 12px;
+    }
+`
+
 export default function Procedimentos(){
     return(
-        <h1>PROCEDIMENTOS</h1>
+        <ProcedimentosStyled>
+            <h1>PROCEDIMENTOS</h1>
+            <Link href='/faciais'>
+                <a>Procedimentos Faciais</a>
+            </Link>
+            <Link href='/corporais'>
+                <a>Procedimentos Corporais</a>
+            </Link>
+        </ProcedimentosStyled>
     )
 }
